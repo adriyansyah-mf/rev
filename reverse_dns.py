bd462b671f1d4c65b7bbd3e5f69f371934f7cdb1
adriyansyah-mf <adriyansyahmf0@gmail.com>
2022-08-30 13:17:58 +0700

    reverse ip

---
+++ reverse_dns.py
@@ -0,0 +1,98 @@
+import dns.resolver
+from fastapi import FastAPI
+
+
+
+
+app = FastAPI()
+
+
+
+
+@app.get("/")
+async def reverse_dns(ip):
+        """Return the reverse DNS entry for an IP address."""
+        dns_type = [
+            'NONE',
+            'A',
+            'NS',
+            'MD',
+            'MF',
+            'CNAME',
+            'SOA',
+            'MB',
+            'MG',
+            'MR',
+            'NULL',
+            'WKS',
+            'PTR',
+            'HINFO',
+            'MINFO',
+            'MX',
+            'TXT',
+            'RP',
+            'AFSDB',
+            'X25',
+            'ISDN',
+            'RT',
+            'NSAP',
+            'NSAP-PTR',
+            'SIG',
+            'KEY',
+            'PX',
+            'GPOS',
+            'AAAA',
+            'LOC',
+            'NXT',
+            'SRV',
+            'NAPTR',
+            'KX',
+            'CERT',
+            'A6',
+            'DNAME',
+            'OPT',
+            'APL',
+            'DS',
+            'SSHFP',
+            'IPSECKEY',
+            'RRSIG',
+            'NSEC',
+            'DNSKEY',
+            'DHCID',
+            'NSEC3',
+            'NSEC3PARAM',
+            'TLSA',
+            'HIP',
+            'CDS',
+            'CDNSKEY',
+            'CSYNC',
+            'SPF',
+            'UNSPEC',
+            'EUI48',
+            'EUI64',
+            'TKEY',
+            'TSIG',
+            'IXFR',
+            'AXFR',
+            'MAILB',
+            'MAILA',
+            'ANY',
+            'URI',
+            'CAA',
+            'TA',
+            'DLV',
+            'RESERVED',
+        ]
+        data = []
+        for record in dns_type:
+            try:
+                print(record)
+                resp = dns.resolver.resolve(ip, record)
+                for rdata in await resp:
+                    data.append(rdata.to_text())
+
+            except:
+                continue
+
+        return {"total": len(data), "data": data}
+
